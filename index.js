391fca3b33d4f2ae5d3304ddff0cad328a59bda6
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-02-07 13:59:35 +0600

    Added token sending method

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+const logger = (req, res, next) => {
+    console.log('inside the logger middleware');
+    next();
+}
 
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    console.log('cookie in the middleware', token);
+
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rinnvkt.mongodb.net/?appName=Cluster0`;
 
@@ -36,10 +51,18 @@ async function run() {
 
         // jwt token related api
         app.post('/jwt', async (req, res) => {
-            const { email } = req.body;
-            const user = { email };
-            const token = jwt.sign(user, 'sccret', { expiresIn: '1h' });
-            res.send({ token });
+            const userData = req.body;
+            const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, {
+                expiresIn: '1d'
+            })
+
+            // set token in the cookies
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+
+            res.send({ success: true });
         })
 
         // all jobs api
@@ -87,7 +110,7 @@ async function run() {
 
         // job applications related APIs
 
-        app.get('/applications', async (req, res) => {
+        app.get('/applications', logger, verifyToken, async (req, res) => {
             const email = req.query.email;
 
             const query = {
